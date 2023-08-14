1973437f49bf71b7576fc0e5a76fa710314d563f
KenjaminButton <kennethpchang@gmail.com>
2023-08-15 02:54:01 +0800

    World Class authentication outsourced to Clerk and completed within 20 minutes of setup time

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className="p-4">
-      <Button size='default' variant='destructive'>Click Me</Button>
+      <p>This is a protected route</p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
+
+export default SetupPage
