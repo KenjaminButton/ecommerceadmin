37a1fa1d7d4d665719fe92a91d3d3a3d44c87376
KenjaminButton <kennethpchang@gmail.com>
2023-08-22 02:44:57 +0800

    Billboard form customized

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -62,9 +62,13 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({initialData}) => {
   const onSubmit = async (data: BillboardFormValues) => {
     try {
       setLoading(true)
-      await axios.patch(`/api/stores/${params.storeId}`, data)
+      if (initialData) {
+        await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data)
+      } else {
+        await axios.post(`/api/${params.storeId}/billboards`, data)
+      }
       router.refresh()
-      toast.success('Store updated')
+      toast.success(toastMessage)
     } catch (error) {
       toast.error('I messed up... Next time it will work')
     } finally {
@@ -75,12 +79,12 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({initialData}) => {
   const onDelete = async () => {
     try {
       setLoading(true)
-      await axios.delete(`/api/stores/${params.storeId}`)
+      await axios.delete(`/api/stores/${params.storeId}/billboards/${params.billboardId}`)
       router.refresh()
       router.push('/')
-      toast.success('Store deleted')
+      toast.success('Billboard deleted')
     } catch(error) {
-      toast.error('Did you remove all products and categories first dumb dumb?')
+      toast.error('Did you remove all categories first dumb dumb?')
     } finally {
       setLoading(false)
       setOpen(false)
