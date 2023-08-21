04b17a581fcbdbd343196f308eecb364cc2c4952
KenjaminButton <kennethpchang@gmail.com>
2023-08-21 16:32:19 +0800

    storeId public api url created as an alert

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -23,6 +23,7 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { AlertModal } from '@/components/modals/alert-modal';
+import { ApiAlert } from '@/components/ui/api-alert';
 
 
 
@@ -125,6 +126,12 @@ const SettingsForm: React.FC<SettingsFormProps> = ({initialData}) => {
 
         </form>
       </Form>
+      <Separator />
+      <ApiAlert 
+        title='NEXT_PUBLIC_API_URL' 
+        description={`${origin}/api/${params.storeId}`}
+        variant='public'
+      />
     </>
     
   )
