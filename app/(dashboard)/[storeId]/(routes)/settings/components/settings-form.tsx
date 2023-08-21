013c8b26d5565d85472325918123d7ccb41b82bb
KenjaminButton <kennethpchang@gmail.com>
2023-08-21 15:56:03 +0800

    Delete functionality created and working

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -62,12 +62,27 @@ const SettingsForm: React.FC<SettingsFormProps> = ({initialData}) => {
     }
   }
 
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(`/api/stores/${params.storeId}`)
+      router.refresh()
+      router.push('/')
+      toast.success('Store deleted')
+    } catch(error) {
+      toast.error('Did you remove all products and categories first dumb dumb?')
+    } finally {
+      setLoading(false)
+      setOpen(false)
+    }
+  }
+
   return (
     <>
       <AlertModal 
         isOpen={open}
         onClose={() => setOpen(false)}
-        onConfirm={() => {}}
+        onConfirm={onDelete}
         loading={loading}
       />
       <div className='flex items-center justify-between'>
